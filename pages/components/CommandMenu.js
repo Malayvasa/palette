b4f06bddade16d9773ea544c0b2aa527458bb8f9
malayvasa <malayvasa@gmail.com>
2023-01-22 18:00:42 +0530

    palette animations

--- pages/components/CommandMenu.js
+++ pages/components/CommandMenu.js
@@ -58,11 +58,11 @@ export default function CommandMenu() {
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                color="#fff"
+                color="#ffffff"
               >
                 <path
-                  d="M21 12v-2a5 5 0 00-5-5H8a5 5 0 00-5 5v0a5 5 0 005 5h4M20.124 19.119a3 3 0 10-4.248-4.237 3 3 0 004.248 4.237zm0 0L22 21"
-                  stroke="#fff"
+                  d="M15.5 15.5L19 19M5 11a6 6 0 1012 0 6 6 0 00-12 0z"
+                  stroke="#ffffff"
                   stroke-width="1.5"
                   stroke-linecap="round"
                   stroke-linejoin="round"
