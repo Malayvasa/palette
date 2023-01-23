b7f121e6d350fd91eb7abe9bef1599faa4f24b81
malayvasa <malayvasa@gmail.com>
2023-01-23 16:52:39 +0530

    fix 2

--- pages/components/Background.js
+++ pages/components/Background.js
@@ -13,7 +13,7 @@ export default function Background({ backgroundColor }) {
 
   return (
     <>
-      <div className="fixed w-screen h-screen top-0 left-0 bg-black -z-20"></div>
+      <div className="fixed w-screen pt-32  h-screen top-0 left-0 bg-black -z-20"></div>
       <AnimatePresence>
         <motion.div
           variants={bg}
@@ -21,9 +21,9 @@ export default function Background({ backgroundColor }) {
           animate="show"
           exit="hidden"
           key={backgroundColor}
-          className="fixed w-screen h-screen top-0 left-0 bg-black -z-10"
+          className="fixed w-screen mt-32 h-screen top-0 left-0 bg-black -z-10"
           style={{
-            background: `radial-gradient(circle at bottom, ${backgroundColor} 0%, rgba(0,0,0,1) 60% )`,
+            background: `radial-gradient(circle at bottom, ${backgroundColor} 0%, rgba(0,0,0,1) 70% )`,
           }}
         ></motion.div>
       </AnimatePresence>
