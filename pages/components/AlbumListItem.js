274ae23af5dc8cf43079baced03970aefc113455
malayvasa <malayvasa@gmail.com>
2023-01-28 11:49:46 +0530

    FIX

--- pages/components/AlbumListItem.js
+++ pages/components/AlbumListItem.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import PaletteExtractor from './PaletteExtractor';
-import Palette from './Palette';
-import AlbumPalette from './AlbumPalette';
+// import React, { useState } from 'react';
+// import PaletteExtractor from './PaletteExtractor';
+// import Palette from './Palette';
+// import AlbumPalette from './AlbumPalette';
 
-export default function AlbumListView({ album }) {
-  const [colors, setColors] = useState([]);
+// export default function AlbumListView({ album }) {
+//   const [colors, setColors] = useState([]);
 
-  return (
-    <div className="flex flex-row justify-center items-center gap-2">
-      <div>
-        <img
-          src={album.images[2].url}
-          alt="Album Cover"
-          width="50"
-          height="50"
-        />
-      </div>
+//   return (
+//     // <div className="flex flex-row justify-center items-center gap-2">
+//     //   <div>
+//     //     <img
+//     //       src={album.images[2].url}
+//     //       alt="Album Cover"
+//     //       width="50"
+//     //       height="50"
+//     //     />
+//     //   </div>
 
-      <PaletteExtractor selectedAlbum={album} setColors={setColors} />
-      <AlbumPalette colors={colors} />
-    </div>
-  );
-}
+//     //   <PaletteExtractor selectedAlbum={album} setColors={setColors} />
+//     //   <AlbumPalette colors={colors} />
+//     // </div>
+//   );
+// }
